104b9d5b9bab4a44ac7102f34d44f15a591ba425
Andrii Vakulenko <Andrii_Vakulenko@epam.com>
2018-05-22 16:56:50 +0300

    home and weather apps components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { WeatherCardComponent } from './weather-card/weather-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    WeatherCardComponent
   ],
   imports: [
     BrowserModule,
